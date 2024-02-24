fae7a79097e1c088862e4226d396cbf714aedb62
aymaneab00 <148016485+aymaneab00@users.noreply.github.com>
2024-02-24 21:08:27 +0000

    Add files via upload

---
+++ task2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int x=3;
+ long long b=12345678912345;
+ char a = 'a';
+ float  c=334.23;
+ double d=14049.30493;
+ cout <<x<<"\n"<<b<<"\n"<<a<<"\n"<<c<<"\n"<<d;
+
+
+}
