232c8394e9c4b7d38f62bf26d3116daa5ea511fb
aymaneab00 <148016485+aymaneab00@users.noreply.github.com>
2024-02-24 21:09:36 +0000

    Add files via upload

---
+++ task4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    int b;
+    int c;
+    int d;
+    cin>>a>>b>>c>>d;
+    cout<<"Difference"<<" "<<"="<<" "<<(a*b)-(c*d);
+    return 0;
+}
