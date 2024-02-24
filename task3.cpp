1887c37492086f427dbd7787975811c4ac72295e
aymaneab00 <148016485+aymaneab00@users.noreply.github.com>
2024-02-24 21:09:05 +0000

    Add files via upload

---
+++ task3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int a = 5;
+int b = 10;
+cout <<a<<" + "<<b<<" = "<<a+b<<"\n"<<a<<" * "<<b<<" = "<<a*b<<"\n"<<a<<" - "<<b<<" = "<<a-b;
+    return 0;
+}
